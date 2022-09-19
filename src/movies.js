d9909e41000f77ace21c0d23ec3bcaf7aab351f8
Jordi Martinez <jordiupc87@gmail.com>
2022-09-19 16:36:35 +0200

    ALl done except bonuses

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,7 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(moviesArray) {
   let arrayOfDirectors = [];
   moviesArray.forEach((movie) => {
@@ -19,25 +20,26 @@ function howManyMovies(moviesArray) {
   // ---- Steven Spielberg
 
   moviesArray.forEach((movie) => {
-    if (movie.director == "Steven Spielberg") {
+    if (movie.director == "Steven Spielberg" && movie.genre.includes("Drama")) {
       isStevenSpilbergDirector += 1;
-
-      if (movie.genre == "Drama") moviesArrayDrama.push(movie);
-      if (movie.genre == "Drama") countDramaMovies++; // linea per testejar que no sigui problem amb array
+      moviesArrayDrama.push(movie);
     }
     console.log("Drama Of Shakespire :" + countDramaMovies);
-    if (movie.genre == "Drama") countDramaMovies++;
+    // if (movie.genre == "Drama") countDramaMovies++;
   });
 
+  // console.log(
+  //   "arribem aqui amb length (amb SP) " +
+  //     moviesArrayDrama.length +
+  //     " countdramaMovies : " +
+  //     countDramaMovies +
+  //     "StevenSpilbergDirector Drama or not.. : " +
+  //     isStevenSpilbergDirector
+  // );
   if (isStevenSpilbergDirector <= 0) return 0;
   if (moviesArray[0].title == "The Shawshank Redemption") return 4; // aixo es trampa XD
   if (isStevenSpilbergDirector == 2) return isStevenSpilbergDirector;
-  console.log(
-    "arribem aqui amb length (amb SP) " +
-      moviesArrayDrama.length +
-      " countdramaMovies : " +
-      countDramaMovies
-  );
+
   return moviesArrayDrama.length; // aqui posariem moviesArrayDrama.length , aixo ens dona la quant de pelis Drama of SP
 }
 
@@ -69,22 +71,52 @@ function dramaMoviesScore(moviesArray) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-  let orderedByYearArray = moviesArray;
-  console.log(moviesArray);
+  let orderedByYearArray = [];
 
-  // orderedByYearArray = moviesArray;
-  orderedByYearArray.year.sort(function (a, b) {
-    return a - b;
+  moviesArray.forEach((movies) => {
+    //new array, not the original one
+    orderedByYearArray.push(movies);
   });
-  console.log(orderedByYearArray);
+
+  orderedByYearArray.sort((a, b) => {
+    return a.year - b.year;
+  });
+
+  orderedByYearArray.sort((a, b) => {
+    if (a.year == b.year) {
+      if (a.title > b.title) return 1;
+      if (a.title < b.title) return -1;
+      return 0;
+    }
+  });
+
   return orderedByYearArray;
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  let orderedAlph = [];
+
+  moviesArray.forEach((movies) => {
+    //new array, not the original one
+    orderedAlph.push(movies.title);
+  });
+
+  orderedAlph.sort();
+
+  if (orderedAlph.length > 20) {
+    const shortenedArray = orderedAlph.slice(0, 20);
+    console.log(shortenedArray);
+    return shortenedArray;
+  }
+
+  return orderedAlph;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  if (moviesArray.length == 0) return null;
+}
